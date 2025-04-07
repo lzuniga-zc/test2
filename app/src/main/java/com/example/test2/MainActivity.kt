c2bb45104fda2a5a706d926bb8a57443ba828dd2
Luis Zuñiga <wolverlazc@hotmail.com>
2025-04-07 12:25:33 -0600

    Update MainActivity.kt

--- app/src/main/java/com/example/test2/MainActivity.kt
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
-                        print("Test 4")
+                        print("Test 5")
                     )
                 }
             }
